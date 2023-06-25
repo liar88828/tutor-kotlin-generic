221dc328adec7fd9da04e74baea8a58fb8b67d19
brian <liar88828@gmail.com>
2023-06-25 22:05:48 +0700

    13 ReadWriteProperty Interface

---
+++ src/main/kotlin/app/ReadWriteProperty.kt
@@ -0,0 +1,28 @@
+package app
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+
+class StringLogReadWriteProperty(var data: String) : ReadWriteProperty<Any, String> {
+	override fun getValue(thisRef: Any, property: KProperty<*>): String {
+		println("Get property ${property.name} with value data")
+		return data
+	}
+
+	override fun setValue(thisRef: Any, property: KProperty<*>, value: String) {
+		println("Set property ${property.name} from $data to $value")
+		data = value
+	}
+}
+
+class Person(params: String) {
+	var name: String by StringLogReadWriteProperty(params)
+}
+
+fun main() {
+	val person = Person("Eko")
+	println(person.name)
+	person.name = "Kurniawan"
+	println(person.name)
+}
